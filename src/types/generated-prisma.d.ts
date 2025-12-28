510e9a6d127583c079b7b721b5ea47b3f7600853
Rodrigo Acevedo <raacevedof@gmail.com>
2025-12-28 14:04:59 +0100

    fixes deploy

--- src/types/generated-prisma.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'generated/prisma' {
-  // Minimal declaration for the generated Prisma client used in this repo
-  export class PrismaClient {
-    constructor();
-    $disconnect(): Promise<void>;
-  }
-  export default PrismaClient;
-}
